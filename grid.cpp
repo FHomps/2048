597a9e622dcdd81a83a0fc609613f67180179923
François HOMPS <francois@dhcp-156-18-28-219.eclair.ec-lyon.fr>
2019-03-25 10:56:29 +0100

    Suppression d'un beep.

--- grid.cpp
+++ grid.cpp
@@ -21,7 +21,6 @@ inline bool within(int i, int min, int max) { return i >= min && i < max; }
 
 void Grid::addTile(Pos pos, unsigned int pow) {
 	if (m_tiles[pos.i][pos.j] == nullptr) {
-		std::cout << "beep" << std::endl;
 		m_tiles[pos.i][pos.j] = new Tile(pos, pow, m_parent);
 	}
 	else {
